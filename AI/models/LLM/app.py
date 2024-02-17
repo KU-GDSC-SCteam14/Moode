a553c09bda6d11bc6a7635896213932431b21697
skitw427 <skitwau@gmail.com>
2024-02-18 05:06:34 +0900

    llm추가

---
+++ AI/models/LLM/app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+from openai import OpenAI
+import openai
+
+openai.api_key = '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'
+
+app = Flask(__name__)
+client = OpenAI(api_key=openai.api_key)
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    diary_entry = request.json['diary_entry']
+    response = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that empathizes with the user's feelings."},
+            {"role": "user", "content": diary_entry}
+        ],
+        model="ft:gpt-3.5-turbo-1106:personal::8s8AiNQE"
+    )
+    return jsonify(response=response.choices[0].message.content) # str형태의 공감 텍스트
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)  # 원하는 포트 번호로 설정
--- AI/models/LLM/fine-tune.py
+++ AI/models/LLM/fine-tune.py
@@ -1,15 +1,19 @@
+import os
 from openai import OpenAI
+import openai
 
-client = OpenAI(
-    api_key="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"
-)
+openai.api_key = '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'
 
-chat_completion = client.chat.completions.create(
+
+
+client = OpenAI(api_key=openai.api_key)
+
+response = client.chat.completions.create(
     messages=[
-        {
-            "role": "user",
-            "content": "오늘 학교에 갔는데 학교가 닫혀있었어.",
-        }
+        {"role": "system", "content": "You are a helpful assistant that empathizes with the user's feelings."},
+        {"role": "user", "content": "재수하고 있던 어느날이었다. 오늘 아침에 씻고 밥먹고 공부하는 곳으로가서 공부하고 오늘도 반복되는 생활을 하였다. 오늘은 사설 모의고사 있는닐이었다. 모의고사를 치고 오답을 하며 오늘하루도 지나갔다."}
     ],
-    model="ftjob-GzmdA9FjF24vv9GNJ5y0txKE",
+    model="ft:gpt-3.5-turbo-1106:personal::8s8AiNQE"
 )
+
+print(response.choices[0].message.content)
