d233f34fd5973f840651907a07fb275d770d7c3f
skitw427 <skitwau@gmail.com>
2024-02-19 00:57:47 +0900

    LLM 추가

--- AI/models/LLM/app.py
+++ AI/models/LLM/app.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, jsonify
 from openai import OpenAI
 import openai
+from dotenv import load_dotenv
+import os
 
-openai.api_key = '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'
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 app = Flask(__name__)
 client = OpenAI(api_key=openai.api_key)
@@ -15,7 +18,7 @@ def chat():
             {"role": "system", "content": "You are a helpful assistant that empathizes with the user's feelings."},
             {"role": "user", "content": diary_entry}
         ],
-        model="ft:gpt-3.5-turbo-1106:personal::8s8AiNQE"
+        model="ft:gpt-3.5-turbo-1106:personal::8tMKeom4"
     )
     return jsonify(response=response.choices[0].message.content) # str형태의 공감 텍스트
 
--- AI/models/LLM/fine-tune.py
+++ AI/models/LLM/fine-tune.py
@@ -1,19 +1,24 @@
 import os
 from openai import OpenAI
 import openai
+from dotenv import load_dotenv
 
-openai.api_key = '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'
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
 
 
 
-client = OpenAI(api_key=openai.api_key)
 
-response = client.chat.completions.create(
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant that empathizes with the user's feelings."},
-        {"role": "user", "content": "재수하고 있던 어느날이었다. 오늘 아침에 씻고 밥먹고 공부하는 곳으로가서 공부하고 오늘도 반복되는 생활을 하였다. 오늘은 사설 모의고사 있는닐이었다. 모의고사를 치고 오답을 하며 오늘하루도 지나갔다."}
-    ],
-    model="ft:gpt-3.5-turbo-1106:personal::8s8AiNQE"
-)
 
-print(response.choices[0].message.content)
+while(True): 
+    text = input("텍스트 입력: ")
+    client = OpenAI(api_key=openai.api_key)
+    response = client.chat.completions.create(
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant that empathizes with the user's feelings."},
+            {"role": "user", "content": text}
+        ],
+        model="ft:gpt-3.5-turbo-1106:personal::8tMKeom4"
+    )
+    print("chat gpt의 응답: ")
+    print(response.choices[0].message.content)
