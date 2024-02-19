d9a4c52bbea459ea7c4d4a312121616e00bf560e
estrellalila <estrellalila@naver.com>
2024-02-19 18:14:00 +0900

    수정

--- FE/mind_care_fe-master/lib/component/body_calendar.dart
+++ FE/mind_care_fe-master/lib/component/body_calendar.dart
@@ -1,36 +1,80 @@
+import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:provider/provider.dart';
 
-class BodyCalendar extends StatelessWidget {
-  final OnDaySelected onDaySelected; // 날짜 선택 시 실행할 함수
-  final DateTime selectedDate; // 선택된 날짜
+class BodyCalendar extends StatefulWidget {
+  @override
+  _BodyCalendar createState() => _BodyCalendar();
+}
 
-  const BodyCalendar({
-    super.key,
-    required this.onDaySelected,
-    required this.selectedDate,
-  });
+class _BodyCalendar extends State<BodyCalendar> {
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  //OnDaySelected onDaySelected; // 날짜 선택 시 실행할 함수
+  DateTime? selectedDate; // 선택된 날짜
+  DateTime? focusedDate;
+
+  @override
+  void initState() {
+    super.initState();
+    selectedDate = focusedDate;
+    _selectedEvents = ValueNotifier(_getEventsForDay(selectedDate!));
+  }
+
+  @override
+  void dispose() {
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  List<Event> _getEventsForDay(DateTime day) {
+    // Implementation example
+    return kEvents[day] ?? [];
+  }
+
+  void onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(selectedDate, selectedDay)) {
+      setState(() {
+        selectedDate = selectedDay;
+        focusedDate = focusedDay;
+      });
+
+      _selectedEvents.value = _getEventsForDay(selectedDay);
+    }
+  }
+
+  // BodyCalendar({
+  //   super.key,
+  //   required this.onDaySelected,
+  //   required this.selectedDate,
+  // });
 
   @override
   Widget build(BuildContext context) {
     // Use the CalendarData instance to get the selected date
     //DateTime selectedDate = Provider.of<CalendarData>(context).selectedDate;
 
-
     return SingleChildScrollView(
       child: Center(
-        child: TableCalendar(
-
+        child: TableCalendar<Event>(
+          calendarBuilders:
+              CalendarBuilders(markerBuilder: (context, date, dynamic event) {
+            if (event.isNotEmpty) {
+              return Container(
+                width: 35,
+                decoration: BoxDecoration(
+                    color: Colors.black, shape: BoxShape.rectangle),
+              );
+            }
+          }),
           onDaySelected: onDaySelected,
           // 날짜 선택 시 실행할 함수
-          selectedDayPredicate: (date) =>
-              date.year == selectedDate.year &&
-              date.month == selectedDate.month &&
-              date.day == selectedDate.day,
+          selectedDayPredicate: (date) => isSameDay(selectedDate, date),
           focusedDay: DateTime.now(), // 화면에 보여지는 날
           firstDay: DateTime(1900, 1, 1), // 첫째 날
           lastDay: DateTime(2999, 12, 31), // 마지막 날
+          eventLoader: _getEventsForDay,
+
           headerStyle: const HeaderStyle(
             // 달력 최상단 스타일
             titleCentered: true,
