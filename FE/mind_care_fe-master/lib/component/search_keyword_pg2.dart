ef6c8e51e7244dc40977be9afdec0fd9e132cc2d
estrellalila <estrellalila@naver.com>
2024-02-21 22:40:13 +0900

    keyword2

--- FE/mind_care_fe-master/lib/component/search_keyword_pg2.dart
+++ FE/mind_care_fe-master/lib/component/search_keyword_pg2.dart
@@ -18,20 +18,12 @@ class _ResultKeyword extends State<ResultKeyword> {
   // ****************!!!! widget.find_keyword (찾으려는 키워드) 기준으로 diaryids 리스트 불러와주세요!!!!
   // ****************  꼭 'widget.find_keyword' 이 형식으로 불러와주세요. 그래야 불려요.
 
-  List<int> diaryIDs = [];
+  late Future<List<int>> diaryIds;
 
   @override
   void initState() {
     super.initState();
-    _loadDiaryIds();
-  }
-
-  Future<void> _loadDiaryIds() async {
-    List<int> loadedDiaryIds =
-        await DatabaseService.getDiaryIdsByKeyword(widget.find_keyword);
-    setState(() {
-      diaryIDs = loadedDiaryIds;
-    });
+    diaryIds = DatabaseService.getDiaryIdsByKeyword(widget.find_keyword);
   }
 
   @override
@@ -58,43 +50,94 @@ class _ResultKeyword extends State<ResultKeyword> {
           centerTitle: true,
         ),
       ),
-      body: Column(
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Container(
-                child: Text(
-                  '\'${widget.find_keyword}\'이 포함된 감정일기',
-                  style: const TextStyle(
-                    fontSize: 17,
-                    fontWeight: FontWeight.bold,
-                  ),
+      body: SafeArea(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Container(
+              child: Text(
+                '\'${widget.find_keyword}\'이 포함된 감정일기',
+                style: const TextStyle(
+                  fontSize: 17,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              Container(
-                height: 32,
-              ),
-              ListView.builder(
-                itemCount: diaryIDs.length,
-                itemBuilder: (context, index) {
-                  return GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ShowDiaryfromSearch(
-                                  diaryID: diaryIDs[index])));
-                    },
-                    child: DiaryCard(
-                      diaryID: diaryIDs[index],
+            ),
+            Container(
+              height: 32,
+            ),
+            // ListView.builder(
+            //   itemCount: diaryIDs.length,
+            //   itemBuilder: (context, index) {
+            //     return GestureDetector(
+            //       onTap: () {
+            //         Navigator.push(
+            //             context,
+            //             MaterialPageRoute(
+            //                 builder: (context) =>
+            //                     ShowDiaryfromSearch(diaryID: diaryIDs[index])));
+            //       },
+            //       child: DiaryCard(
+            //         diaryID: diaryIDs[index],
+            //       ),
+            //     );
+            //   },
+            // )
+            FutureBuilder<List<int>>(
+              future: diaryIds,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  return Text('Error: ${snapshot.error}');
+                } else if (snapshot.hasData) {
+                  List<int> diaryIDs = snapshot.data!;
+                  return Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xffd6dadc),
+                      borderRadius: BorderRadius.circular(18),
+                    ),
+                    child: Column(
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.only(top: 16),
+                          child: Column(
+                            children: [
+                              ListView.builder(
+                                shrinkWrap:
+                                    true, // 추가된 부분: ListView가 부모 위젯의 크기에 맞게 조절
+                                physics:
+                                    const NeverScrollableScrollPhysics(), // 추가된 부분: 스크롤을 방지
+                                itemCount: diaryIDs.length,
+                                itemBuilder: (context, index) {
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) =>
+                                                  ShowDiaryfromSearch(
+                                                      diaryID:
+                                                          diaryIDs[index])));
+                                    },
+                                    child: DiaryCard(diaryID: diaryIDs[index]),
+                                  );
+                                },
+                              ),
+                              const SizedBox(height: 8),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   );
-                },
-              )
-            ],
-          ),
-        ],
+                } else {
+                  return const Text('No data available');
+                }
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
--- FE/mind_care_fe-master/lib/screen/home_screen.dart
+++ FE/mind_care_fe-master/lib/screen/home_screen.dart
@@ -11,17 +11,6 @@ import 'package:provider/provider.dart';
 import 'package:mind_care/notification.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class CalendarData extends ChangeNotifier {
-  DateTime _selectedDate = DateTime.now();
-
-  DateTime get selectedDate => _selectedDate;
-
-  void updateSelectedDate(DateTime newDate) {
-    _selectedDate = newDate;
-    notifyListeners();
-  }
-}
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -39,79 +28,74 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-        create: (context) => CalendarData(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: const Text('Mind Care'),
-            centerTitle: true,
-            leading: IconButton(
-                // 간단한 위젯이나 타이틀들을 앱바의 왼쪽에 위치시키는 것을 말함
-                icon: const Icon(Icons.menu), // 아이콘
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Setting()),
-                  );
-                }),
-            actions: [
-              IconButton(
-                  icon: const Icon(Icons.search),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => SearchKeyword()),
-                    );
-                  }),
-            ],
-          ),
-          body: SafeArea(
-            child: ListView(
-              children: [
-                // 주간 긍정일기 확인버튼 배너
-                Container(
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Text('주간 긍정일기를 확인해보세요.'),
-                        TextButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => HappyDiaryScreen()),
-                              );
-                            },
-                            child: Text('확인하기')),
-                      ]),
-                ),
-
-                BodyCalendar(
-                  selectedDate: selectedDate, // 날짜 전달하기
-                  onDaySelected: onDaySelected, // 날짜 선택됐을 때 실행할 함수
-                ),
-                SelectedDiaryList(
-                  selectedDate: selectedDate,
-                ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton.extended(
-            backgroundColor: const Color.fromRGBO(0, 122, 255, 1.0),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Mind Care'),
+        centerTitle: true,
+        leading: IconButton(
+            // 간단한 위젯이나 타이틀들을 앱바의 왼쪽에 위치시키는 것을 말함
+            icon: const Icon(Icons.menu), // 아이콘
             onPressed: () {
               Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Setting()),
+              );
+            }),
+        actions: [
+          IconButton(
+              icon: const Icon(Icons.search),
+              onPressed: () {
+                Navigator.push(
                   context,
-                  MaterialPageRoute(
-                      builder: (context) => const WriteExperience()));
-            },
-            icon: const Icon(
-              Icons.add,
+                  MaterialPageRoute(builder: (context) => SearchKeyword()),
+                );
+              }),
+        ],
+      ),
+      body: SafeArea(
+        child: ListView(
+          children: [
+            // 주간 긍정일기 확인버튼 배너
+            Container(
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Text('주간 긍정일기를 확인해보세요.'),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HappyDiaryScreen()),
+                          );
+                        },
+                        child: Text('확인하기')),
+                  ]),
             ),
-            label: const Text('슬라이드 해 오늘의 일기를 작성하세요.'),
-          ),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerFloat,
-        ));
+
+            BodyCalendar(
+              selectedDate: selectedDate, // 날짜 전달하기
+              onDaySelected: onDaySelected, // 날짜 선택됐을 때 실행할 함수
+            ),
+            SelectedDiaryList(
+              selectedDate: selectedDate,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: const Color.fromRGBO(0, 122, 255, 1.0),
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const WriteExperience()));
+        },
+        icon: const Icon(
+          Icons.add,
+        ),
+        label: const Text('슬라이드 해 오늘의 일기를 작성하세요.'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 
   void onDaySelected(DateTime selectedDate, DateTime focusedDate) {
