acd2d7d2f01b8c81ab9c0d97c43d88903e462889
Junhyoung <junh3951@me.com>
2024-03-07 03:40:58 +0900

    Update app.js

--- BE/app.js
+++ BE/app.js
@@ -87,7 +87,7 @@ cron.schedule('* * * * *', async () => {
 
   function getTomorrow() {
     const days = ['일요일', '월요일', '화요일', '수요일', '목요일', '금요일', '토요일'];
-    const todayIndex = new Date().getDay();
+    const todayIndex = new Date().getDay() + 1;
     const tomorrowIndex = (todayIndex + 0) % 7;
     console.log(days[tomorrowIndex]);
     return days[tomorrowIndex];
